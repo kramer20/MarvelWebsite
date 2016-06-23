1906ee800e61ea4f57178ff215690e07fe6140d3
Malorie Kramer <maloriekramer@Malorie-Kramers-MacBook-Pro.local>
2016-06-22 22:55:31 -0400

    price

--- js/comics.js
+++ js/comics.js
@@ -21,11 +21,25 @@ $("body").on("click",".moreInfo", function(){
 });
 
 
+function buildPrices(prices){
+  var bigString = "";
+    //to set how many comics you want to show
+  //var counter = 0;
+  prices.forEach(function(price){
+    //if(counter<=5){
+    bigString += `<p>${price.price}</p>`;
+   //counter++;
+    });
+
+  return bigString;
+}
 
 //loop that writes to HTML
     comics.forEach(function (comic){
     	var comicImage = comic.thumbnail.path+"."+comic.thumbnail.extension;
-    	$(".comicTwoRow").append('<div class="col-lg-4 comic leftTop"><div class="column secondLeft"><img class="comicImage" src='+comicImage+'></div><h3>'+comic.title+'</h3><div class="moreInfo"><button class="btn btn-primary moreInfo" type="button">More Info</button></div><div class="collapse moreInfoDiv" id="collapseExample"><div class="card card-block"><p class="comicDescription">'+comic.description+'</p></div></div>');
+      var comicPrice = comic.prices;
+      comicPrice = buildPrices(comicPrice);
+    	$(".comicTwoRow").append('<div class="col-lg-4 comic leftTop"><div class="column secondLeft"><img class="comicImage" src='+comicImage+'></div><h3>'+comic.title+'</h3><div class="moreInfo"><button class="btn btn-primary moreInfo" type="button">More Info</button></div><div class="collapse moreInfoDiv" id="collapseExample"><div class="card card-block"><span>$</span><p class= "comicPrice">'+comicPrice+'</p><p class="comicDescription">'+comic.description+'</p></div></div>');
 
     });
 
